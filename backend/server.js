ec09404c38062f00ca8894d8489ab5dd7ef0c760
monirulHaque <monirul.haque@g.bracu.ac.bd>
2021-01-27 19:40:09 +0600

    Set Express Routes to serve products through api

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,19 @@
 const express = require("express");
+const products = require("./data/products");
+
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API Homepage");
+});
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/product/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
 
 app.listen(4000, console.log("Server is running on port 4000!"));
