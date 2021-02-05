40022baea893b036da688cc2660d1eac48e4ae90
monirulHaque <monirul.haque@g.bracu.ac.bd>
2021-02-05 15:14:48 +0600

    Working database using clever cloud

--- backend/data/users.js
+++ backend/data/users.js
@@ -4,23 +4,23 @@ const users = [
   {
     name: "Admin User",
     email: "admin@gmail.com",
-    passsword: bcrypt.hashSync("123456", 10),
+    password: bcrypt.hashSync("123456", 10),
     isAdmin: true,
   },
   {
     name: "Monirul",
     email: "monirul@gmail.com",
-    passsword: bcrypt.hashSync("monirul123", 10),
+    password: bcrypt.hashSync("monirul123", 10),
   },
   {
     name: "Kamru",
     email: "kamru@gmail.com",
-    passsword: bcrypt.hashSync("kamru69", 10),
+    password: bcrypt.hashSync("kamru69", 10),
   },
   {
     name: "Captain Levi",
     email: "caplevi@yahoo.com",
-    passsword: bcrypt.hashSync("levi1234", 10),
+    password: bcrypt.hashSync("levi1234", 10),
   },
 ];
 
--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -31,7 +31,7 @@ const productSchema = mongoose.Schema(
       type: String,
       required: true,
     },
-    catagory: {
+    category: {
       type: String,
       required: true,
     },
